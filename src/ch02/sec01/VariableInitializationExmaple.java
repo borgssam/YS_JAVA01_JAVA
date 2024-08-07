62b72d9a2520ebe39c4852477cc2f0ed3c4a6bc0
borgssam <borgssam@gmail.com>
2024-08-07 16:28:32 +0900

    2장 변수1

---
+++ src/ch02/sec01/VariableInitializationExmaple.java
@@ -0,0 +1,13 @@
+package ch02.sec01;
+
+public class VariableInitializationExmaple {
+
+	public static void main(String[] args) {
+		//변수 value선언
+		int value = 0;
+		int result = value + 10;
+		System.out.println(result);
+
+	}
+
+}
