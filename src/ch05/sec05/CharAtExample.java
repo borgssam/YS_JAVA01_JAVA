c791adf919decfd8d096b8d65b90fc61f03c5318
borgssam <borgssam@gmail.com>
2024-08-12 15:51:18 +0900

    5장 참조타입2

---
+++ src/ch05/sec05/CharAtExample.java
@@ -0,0 +1,20 @@
+package ch05.sec05;
+
+public class CharAtExample {
+
+	public static void main(String[] args) {
+		String juminNum = "011222-3051454";
+		char szSex = juminNum.charAt(7);
+
+
+		
+		char[] juminArr = {'0','1','1','2','2','2','-','3','0','5','1','4','5','4'};		
+		char sex2 = juminArr[7];
+		System.out.println(sex2);
+
+		
+		
+
+	}
+
+}
