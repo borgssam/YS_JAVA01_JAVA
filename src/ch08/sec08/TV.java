92fd8d9dbfd59789f68c806b05d78795d528c243
borgssam <borgssam@gmail.com>
2024-08-19 12:22:03 +0900

    8장 인터페이스3

---
+++ src/ch08/sec08/TV.java
@@ -0,0 +1,18 @@
+package ch08.sec08;
+
+public class TV implements RemoteControl {
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		System.out.println("TV on");
+
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("TV off");
+
+	}
+
+}
